50be91474b607abce0dcb03ef3e54560b656c71e
andrew1017 <115038998+andrew1017@users.noreply.github.com>
2025-05-08 15:23:46 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://andrew1017.github.io/demo-github-actions/",
+  site: "https://andrew1017.github.io",
   base: "/demo-github-actions",
 });
